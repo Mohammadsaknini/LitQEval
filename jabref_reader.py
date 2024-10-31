bfd7ec6fdbd9f17d35c2b2f5ff73f408662b5c70
Mohammad Saknini <mohammad.saknini@int.fraunhofer.de>
2024-10-31 11:39:56 +0100

    Extracted metadata from dimensions

--- jabref_reader.py
+++ jabref_reader.py
@@ -1,9 +1,43 @@
 import bibtexparser
 import pandas as pd
+import numpy as np
+import dimcli
+import json
 
+class NumpyEncoder(json.JSONEncoder):
+    """
+    Converts a numpy array to a json object:
+    >>> array = np.zeros(10)
+    >>> array_as_json = json.dumps(a,cls=NumpyEncoder)
+    """
 
+    def default(self, obj) -> dict:
+        """
+        If input object is an ndarray it will be converted into a dict holding dtype, shape and the data, base64 encoded.
 
-def data_to_df():
+        paramters
+        ---------
+        obj: object
+            object to be converted into a json object
+
+        returns
+        -------
+        dict
+            dictionary holding the dtype, shape and the data, base64 encoded.
+        """
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+    
+def jabref_to_excel() -> pd.DataFrame:
+    """
+    Read the JabRef file and extract the core publications for each group
+
+    Returns
+    -------
+    df: pd.DataFrame
+        The DataFrame containing the core publications for each group
+    """
     GROUPS = {
         "AI on Edge Devices": ["", [], []],  # Artificial Intelligence
         "Synthetic Biology": ["", [], []],  # Biology
@@ -21,7 +55,7 @@ def data_to_df():
         "Green Warehousing": ["", [], []],  # Climate Science
         "Internet of Things in Healthcare": ["", [], []],  # Internet of Things
     }
-    with open("eval_datasets.bib", encoding="utf-8") as bibtex_file:
+    with open("data/publications.bib", encoding="utf-8") as bibtex_file:
         bibtex_database = bibtexparser.load(bibtex_file)
         for entry in bibtex_database.entries:
             group = entry["groups"]
@@ -33,13 +67,57 @@ def data_to_df():
                 GROUPS[group][1].append(pub_id)
                 GROUPS[group][2].append(title)
 
-            
-
     df = pd.DataFrame(GROUPS).T
     df.reset_index(inplace=True)
     df.rename(columns={0: "Survey", 1: "Core Publications", 2: "Title", "index": "Group"}, inplace=True)
     df = df.explode(["Core Publications", "Title"], ignore_index=True)
     return df
 
-df = data_to_df()
-df.to_excel("eval_datasets.xlsx", index=False)
+def get_query(attributes: str, pub_ids: str):
+    """
+    Generate the query for the Dimensions API
+
+    Parameters
+    ----------
+    attributes: list
+        The list of attributes to extract
+    publication_ids: list
+        A list valid dimensions publication ids
+
+    Returns
+    -------
+    query: str
+        The generated query
+    """
+    
+    pub_ids = json.dumps(pub_ids, cls=NumpyEncoder)
+    attributes = "+".join(attributes)
+    query = f"""search publications where id in {pub_ids} return publications[{attributes}]"""
+    return query
+
+def extract_metadata(df: pd.DataFrame) -> pd.DataFrame:
+    dimcli.login()
+    dsl = dimcli.Dsl()
+    pub_ids = list(set(df["Core Publications"].tolist() + df["Survey"].tolist()))
+    chunks = []
+    data = []
+    attirbutes = ["id","title", "doi", "abstract", "times_cited", "field_citation_ratio", "year"]
+    if len(pub_ids) > 300:
+        for i in range(0, len(pub_ids), 300):
+            chunks.append(pub_ids[i:i+300])
+    else:
+        chunks.append(pub_ids)
+
+    for chunk in chunks:
+        query = get_query(attirbutes, chunk)
+        response = dsl.query_iterative(query) # type: dimcli.DslDataset
+        data += response.publications
+
+    return pd.DataFrame(data)[attirbutes]
+
+if __name__ == "__main__":
+    df = jabref_to_excel()
+    metadata = extract_metadata(df)
+    metadata.to_excel("data/metadata.xlsx", index=False)
+    df.to_excel("data/core_publications.xlsx", index=False)
+    
